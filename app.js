ea80679d56a311e11a729069a51e1e8511d652fe
Meulemans Philippe <90241977+Laverdure77@users.noreply.github.com>
2022-10-14 15:57:52 +0200

    Add & Config ESLint

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
+
 const sequelize = require('./sources/db/sequelize.js');
 
 const app = express();
-const port = 3000;
+const port = 3000 ;
 
 app
-    .use(favicon(__dirname + '/favicon.ico'))
-    .use(bodyParser.json())
+	.use(favicon(__dirname + '/favicon.ico'))
+	.use(bodyParser.json());
 
 sequelize.initDb();
 
-require('./sources/routes/findAllPokemons')(app); // pas dbesoin de varaible intermédiaire.
+require('./sources/routes/findAllPokemons')(app); // No need of intermediate variable
 require('./sources/routes/findPokemonByPk')(app);
 require('./sources/routes/createPokemon')(app);
 require('./sources/routes/updatePokemon')(app);
@@ -20,10 +22,10 @@ require('./sources/routes/deletePokemon')(app);
 require('./sources/routes/login')(app);
 
 
-// ERROR 404 for all routes declared, if user tries to access an non existing route
+// ERROR 404 for all routes declared, if user tries to access an non existing route.
 app.use(({res}) => {
-    const message = 'impossible de trover la ressource demandée, essayez une autre URL.';
-    res.status(404).json(message);
-})
+	const message = 'impossible de trouver la ressource demandée, essayez une autre URL.';
+	res.status(404).json(message);
+});
 
 app.listen(port, () => console.log(`Notre app Node est démarrée sur : http://localhost:${port}`));
